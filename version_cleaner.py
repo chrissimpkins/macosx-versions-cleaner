d9ef42520e5e8102a795dac121a2582218a330c8
Chris Simpkins <git.simpkins@gmail.com>
2015-06-02 23:09:24 -0400

    indentation cleanup

--- version_cleaner.py
+++ version_cleaner.py
@@ -21,31 +21,31 @@ def execute(command):
 versions_dir = "/.DocumentRevisions-V100"
 
 if os.path.isdir(versions_dir):
-	print("Versions directory size before cleaning:")
-	execute_command = "du -sh " + versions_dir
-	execute(execute_command)
-	print(" ")
+    print("Versions directory size before cleaning:")
+    execute_command = "du -sh " + versions_dir
+    execute(execute_command)
+    print(" ")
 
-	try:
-	    shutil.rmtree(versions_dir)
-	except Exception as e:
-		print("Error in the attempt to remove the files. Error message: " + str(e))
-		sys.exit(1)
+    try:
+        shutil.rmtree(versions_dir)
+    except Exception as e:
+        print("Error in the attempt to remove the files. Error message: " + str(e))
+        sys.exit(1)
 
-	if not os.path.isdir(versions_dir):
-		print("The versions directory was deleted.")
+    if not os.path.isdir(versions_dir):
+        print("The versions directory was deleted.")
 
-		if sys.version_info[0] == 2:
-			user_response = raw_input("Reboot to complete cleanup? (y/n): ")
-		else:
-			user_response = input("Reboot to complete cleanup? (y/n): ")
+        if sys.version_info[0] == 2:
+            user_response = raw_input("Reboot to complete cleanup? (y/n): ")
+        else:
+            user_response = input("Reboot to complete cleanup? (y/n): ")
 
-		if user_response.lower() == "y":
-			print("Rebooting...")
-			execute("reboot")
-		else:
-			print("Skipping system reboot.  Please reboot your system manually to begin using document versioning again.")
+        if user_response.lower() == "y":
+            print("Rebooting...")
+            execute("reboot")
+        else:
+            print("Skipping system reboot.  Please reboot your system manually to begin using document versioning again.")
 else:
-	print("Unable to locate a versions directory at the path '" + versions_dir + "'. It appears that it was already cleaned.")
-	print("Complete the cleanup process by rebooting your system.")
+    print("Unable to locate a versions directory at the path '" + versions_dir + "'. It appears that it was already cleaned.")
+    print("Complete the cleanup process by rebooting your system.")
 
